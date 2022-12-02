93f76087bf28bba60b1432bfe99a0bfaa9dc237c
kashine <>
2022-12-02 14:16:36 +0900

    TailwindCSS / clsx

--- README.md
+++ README.md
@@ -8,18 +8,8 @@ This includes Next.js' built-in support for Global CSS, CSS Modules and TypeScri
 
 Quickly get started using [Create Next App](https://github.com/vercel/next.js/tree/canary/packages/create-next-app#readme)!
 
-In your terminal, run the following command:
-
 ```bash
-npx create-next-app --example with-jest with-jest-app
-```
-
-```bash
-yarn create next-app --example with-jest with-jest-app
-```
-
-```bash
-pnpm create next-app --example with-jest with-jest-app
+yarn create next-app --example with-jest .
 ```
 
 ## Run Jest Tests
@@ -27,3 +17,22 @@ pnpm create next-app --example with-jest with-jest-app
 ```bash
 npm test
 ```
+
+---
+
+## setup
+
+- Eslint/Prettier
+- src directory
+- Path Alias
+
+## CSS
+
+- Tailwind CSS
+  https://tailwindcss.com/docs/guides/nextjs
+
+- tailwind prettier
+  https://github.com/tailwindlabs/prettier-plugin-tailwindcss
+
+- clsx
+  https://www.npmjs.com/package/clsx
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,57 @@
 import Head from 'next/head'
-import Image from 'next/image'
 
 import styles from '@/pages/index.module.css'
+import clsx from 'clsx'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main
+        className={clsx('flex min-h-screen flex-col items-center justify-center gap-4 py-8 px-0')}
+      >
+        <h1 className='text-3xl font-bold underline'>Next.js + TanStack Query Tips</h1>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
+        <div className='flex max-w-3xl flex-wrap items-center justify-center gap-2'>
+          <a
+            href='https://nextjs.org/docs'
+            className='w-2/3 rounded border p-6 text-left no-underline'
+          >
+            <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
+          <a
+            href='https://nextjs.org/learn'
+            className='w-2/3 rounded border p-6 text-left no-underline'
+          >
+            <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+            href='https://github.com/vercel/next.js/tree/canary/examples'
+            className='w-2/3 rounded border p-6 text-left no-underline'
           >
-            <h3>Examples &rarr;</h3>
+            <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
-          <a href="https://vercel.com/new" className={styles.card}>
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a
+            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='w-2/3 rounded border p-6 text-left no-underline'
+          >
+            <h2>Deploy &rarr;</h2>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
